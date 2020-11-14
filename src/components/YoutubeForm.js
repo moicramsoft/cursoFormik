fe1a244071b23cf36b4ea8862983f0411643a13d
Marcio Silva de Oliveira <51038546+moicramsoft@users.noreply.github.com>
2020-11-14 18:59:16 -0300

    Visita aos campos do form

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -36,26 +36,27 @@ function YoutubeForm() {
        })
        // Ao habilitar linha abaixo mostramos na console os valores que foram digitados pelo usuário
        //console.log('Form value', formik.values)
-       
-       console.log('Form errors', formik.errors)
+       // Exibira os erros 
+       //console.log('Form errors', formik.errors)
+       console.log('Visited Fields', formik.touched)
 
     return (
         <div>
             <form onSubmit={formik.handleSubmit}>
                 <div className='form-control'>
                 <label htmlFor='name'>Name</label>
-                <input type='text' id='name' name='name'      onChange={formik.handleChange} vale={formik.values.name}/>
+                <input type='text' id='name' name='name'      onChange={formik.handleChange} vale={formik.values.name} onBlur={formik.handleBlur}/>
                {formik.errors.name?<div className='error'>{formik.errors.name}</div> : null}
                </div>
                <div className='form-control'>
 
                 <label htmlFor='email'>E-mail</label>
-                <input type='text' id='email' name='email'    onChange={formik.handleChange} vale={formik.values.email}/>
+                <input type='text' id='email' name='email'    onChange={formik.handleChange} vale={formik.values.email} onBlur={formik.handleBlur}/>
                 {formik.errors.email?<div className='error'>{formik.errors.email}</div> : null}
                 </div>
                <div className='form-control'>
                 <label htmlFor='channel'>Channel</label>
-                <input type='text' id='channel' name='channel'onChange={formik.handleChange} vale={formik.values.channel}/>
+                <input type='text' id='channel' name='channel'onChange={formik.handleChange} vale={formik.values.channel} onBlur={formik.handleBlur}/>
                 {formik.errors.channel?<div className='error'>{formik.errors.channel}</div> : null}
                 </div>
        <button type='submit' >Submit</button>
