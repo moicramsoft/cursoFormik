cb86d89e4c8f414077b433c29a18590083f45ee6
Marcio Silva de Oliveira <51038546+moicramsoft@users.noreply.github.com>
2020-11-14 21:32:24 -0300

    Erros tratados com Yup

---
+++ src/components/OldYoutubeForm1.js
@@ -0,0 +1,102 @@
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+/*Aqui podemos setar valores iniciais 
+        Na console do Browser será exibido os valores digitados devidos ao 
+        onChange={formik.handleChange} vale={formik.values.name}*/
+const initialValues = {
+  name: "",
+  email: "",
+  channel: "",
+};
+/* Ao pressionar o submit exibimos os valores na console do Browser
+ */
+const onSubmit = (values) => {
+  console.log("Form values", values);
+};
+
+const validate = (values) => {
+  let errors = {};
+  if (!values.name) {
+    errors.name = "Required!";
+  }
+  if (!values.email) {
+    errors.email = "Required!";
+  }
+  if (!values.channel) {
+    errors.channel = "Required!";
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = "Invalid email format!";
+  }
+  return errors;
+};
+const validationSchema = Yup.object({
+  name: Yup.string().required("Required!"),
+  email: Yup.string().email("Invalid email format").required("Required!"),
+  channel: Yup.string().required("Required!"),
+});
+function OldYoutubeForm1() {
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+    validationSchema,
+    // validate
+  });
+  // Ao habilitar linha abaixo mostramos na console os valores que foram digitados pelo usuário
+  //console.log('Form value', formik.values)
+  // Exibira os erros
+  //console.log('Form errors', formik.errors)
+  console.log("Visited Fields", formik.touched);
+
+  return (
+    <div>
+      <form onSubmit={formik.handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            onChange={formik.handleChange}
+            vale={formik.values.name}
+            onBlur={formik.handleBlur}
+          />
+          {formik.touched.name && formik.errors.name ? (
+            <div className="error">{formik.errors.name}</div>
+          ) : null}
+        </div>
+        <div className="form-control">
+          <label htmlFor="email">E-mail</label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            onChange={formik.handleChange}
+            vale={formik.values.email}
+            onBlur={formik.handleBlur}
+          />
+          {formik.touched.email && formik.errors.email ? (
+            <div className="error">{formik.errors.email}</div>
+          ) : null}
+        </div>
+        <div className="form-control">
+          <label htmlFor="channel">Channel</label>
+          <input
+            type="text"
+            id="channel"
+            name="channel"
+            onChange={formik.handleChange}
+            vale={formik.values.channel}
+            onBlur={formik.handleBlur}
+          />
+          {formik.touched.channel && formik.errors.channel ? (
+            <div className="error">{formik.errors.channel}</div>
+          ) : null}
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default OldYoutubeForm1;
--- src/components/OldYoutubeForm2.js
+++ src/components/OldYoutubeForm2.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { useFormik } from "formik";
 import * as Yup from "yup";
 /*Aqui podemos setar valores iniciais 
         Na console do Browser será exibido os valores digitados devidos ao 
@@ -20,40 +20,62 @@ const validationSchema = Yup.object({
   email: Yup.string().email("Invalid email format").required("Required!"),
   channel: Yup.string().required("Required!"),
 });
-function YoutubeForm() {
+function OldYoutubeForm2() {
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+    validationSchema,
+    // validate
+  });
   // Ao habilitar linha abaixo mostramos na console os valores que foram digitados pelo usuário
   //console.log('Form value', formik.values)
   // Exibira os erros
   //console.log('Form errors', formik.errors)
-  //console.log("Visited Fields", formik.touched);
+  console.log("Visited Fields", formik.touched);
 
   return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={validationSchema}
-      onSubmit={onSubmit}
-    >
-      <Form>
+    <div>
+      <form onSubmit={formik.handleSubmit}>
         <div className="form-control">
           <label htmlFor="name">Name</label>
-          <Field type="text" id="name" name="name" />
-          <ErrorMessage name='name' />
+          <input
+            type="text"
+            id="name"
+            name="name"
+            {...formik.getFieldProps("name")}
+          />
+          {formik.touched.name && formik.errors.name ? (
+            <div className="error">{formik.errors.name}</div>
+          ) : null}
         </div>
         <div className="form-control">
           <label htmlFor="email">E-mail</label>
-          <Field type="text" id="email" name="email" />
-          <ErrorMessage name='email' />
+          <input
+            type="text"
+            id="email"
+            name="email"
+            {...formik.getFieldProps("email")}
+          />
+          {formik.touched.email && formik.errors.email ? (
+            <div className="error">{formik.errors.email}</div>
+          ) : null}
         </div>
         <div className="form-control">
           <label htmlFor="channel">Channel</label>
-          <Field type="text" id="channel" name="channel" />
-          <ErrorMessage name='channel' />
-
+          <input
+            type="text"
+            id="channel"
+            name="channel"
+            {...formik.getFieldProps("channel")}
+          />
+          {formik.touched.channel && formik.errors.channel ? (
+            <div className="error">{formik.errors.channel}</div>
+          ) : null}
         </div>
         <button type="submit">Submit</button>
-      </Form>
-    </Formik>
+      </form>
+    </div>
   );
 }
 
-export default YoutubeForm;
+export default OldYoutubeForm2;
